7c03fc1f37d810245325581f9c15f26adc05b62b
Sandra Quero <sanquesan@gmail.com>
2021-10-07 18:33:30 +0200

    Add code to all js files in src

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     "no-restricted-syntax": "off",
     "consistent-return": "off",
     "no-debugger": "off",
+    "no-console": "off",
   },
 };
---
+++ src/skylabArray.js
@@ -0,0 +1,34 @@
+class SkylabArray {
+  constructor(valor) {
+    this.length = valor;
+  }
+
+  push(valor) {
+    const arrayTest = [];
+    return arrayTest.push(this.valor);
+  }
+
+  some(valor) {
+    //
+  }
+
+  find(valor) {
+    //
+  }
+
+  filter(valor) {
+    //
+  }
+
+  map(valor) {
+    //
+  }
+
+  module.exports = {
+    push,
+    some,
+    find,
+    filter,
+    map,
+  };
+}
