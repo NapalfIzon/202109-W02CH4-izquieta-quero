aadcd42a200565ab80048b1ecbf0a20fb1fe9ea8
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-10-07 23:02:34 +0200

    :memo:  add find method in SkylabArray class and test

--- src/skylabArray.test.js
+++ src/skylabArray.test.js
@@ -43,3 +43,28 @@ describe("Testeo de la propiedad push de la clase SkylabArray", () => {
     expect(result).toEqual(expected);
   });
 });
+
+describe("Testeo de la propiedad push de la clase SkylabArray", () => {
+  test("Cuando le pasamos 45, deberia devolver [12, 45]", () => {
+    const valor = 45;
+    const expected = [12, 45];
+
+    const testPush = new SkylabArray([12]);
+    const result = testPush.push(valor);
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("Testeo de la propiedad find de la clase SkylabArray", () => {
+  test("Cuando con un array [4, 8, 15, 16, 23, 42]le pasamos la expresión 'element => element === 23', deberia devolver 23", () => {
+    const valor = (element) => element === 23;
+    const array = [4, 8, 15, 16, 23, 42];
+    const expected = 23;
+
+    const testFind = new SkylabArray(array);
+    const result = testFind.find(valor);
+
+    expect(result).toBe(expected);
+  });
+});
